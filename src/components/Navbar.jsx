c0fe5e2214c9c66fffd7ec15c38038b1dea99d5f
Rebeca Gasso <Rebeca_Gasso@gap.com>
2024-02-11 18:34:30 -0300

    feat: home

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink to="/" className="w-40 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <p className="purple-gradient_text">Rebeca Gasso</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/about" className={({isActive}) => isActive ? 'text-purple-800': 'text-black'}>
+          About
+        </NavLink>
+        <NavLink to="/contact" className={({isActive}) => isActive ? 'text-purple-800': 'text-black'}>
+          Contact
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React, {Suspense} from 'react';
+import Loader from "../components/Loader";
+import logo from "../assets/mainImage.png";
+
+{/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">*/}
+{/*  POPUP*/}
+{/*</div>*/}
+
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative background flex flex-col items-center items-center gap-y-10 ">
+        <Suspense fallback={<Loader />}>
+          <div className="row-span-1 pt-20">
+            <img className="h-96 w-96 shadow-xs content-center justify-center" src={logo} alt="Logo" />
+          </div>
+          <h2 className="text-2xl animate-bounce duration-75">Hi, I'm Rebeca!</h2>
+          <div className="row-span-1 w-2/4 text-xl font-mono">
+            <p className="">
+              Software Engineer with six years of software development experience.
+              Started getting interested in web development when I was a teen building my own theme for tumblr.
+            </p>
+            <p>
+              I have experience in companies that include Cloud
+              Hosting, E-commerce and Design System. I love Front-End development, my main technology are
+              React/Typescript and CSS.
+            </p>
+          </div>
+
+        </Suspense>
+    </section>
+  );
+};
+
+export default Home;
